962d7c16aaf5d05433d97a54d755ebcbbe8fd38d
ahmadun <ahmadun.jambi@gmail.com>
2023-01-16 20:11:57 +0700

    initializing

---
+++ DemoApp.Net5.0/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoAppNet5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        #region Public Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+#if DEBUG
+            AppDomain.CurrentDomain.FirstChanceException += (source, e) =>
+            {
+                Debug.WriteLine("FirstChanceException event raised in {0}: {1}\r\n{2}",
+                 AppDomain.CurrentDomain.FriendlyName, e.Exception.Message, source);
+            };
+#endif
+            var viewModel = new ModelView.ModelView(100_000);
+
+            DataContext = viewModel;
+        }
+
+        #endregion Public Constructors
+    }
+}
